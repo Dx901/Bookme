f59754ac68bd284777da8ffc5bb9b7cf3bd95aea
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-12-06 17:10:08 +0300

    configured the ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  resources :booking_types
-  resources :bookin_types
-  resources :bookings
+
     authenticate :user, lambda { |u| u.admin? } do
       mount Sidekiq::Web => '/sidekiq'
     end
-
 
   devise_for :users
 
   authenticated :user do
     root to: "home#dashboard", as: :authenticated_root
-    # Says we have home controller with a action called dashboard
   end
 
-  root to: 'home#index'
- 
-end
+  resources :booking_types
+  resources :bookings
 
-# Use a built-in routing method by the devise gem
-# The authentiacted :user checks if a user is indeed signed in 
-# and if so redirected to a new root path
+  get ":booking_link", to: "users#show", as: :user
+
+  root to: "home#index"
+end
