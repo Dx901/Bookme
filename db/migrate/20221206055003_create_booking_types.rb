9c92e8e20e326844ce81d753d7cca4f0a1bd0992
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-12-06 09:53:44 +0300

    Added models

--- db/migrate/20221206055003_create_booking_types.rb
+++ db/migrate/20221206055003_create_booking_types.rb
@@ -3,9 +3,9 @@ class CreateBookingTypes < ActiveRecord::Migration[7.0]
     create_table :booking_types do |t|
       t.string :name
       t.string :location
-      t.string :color
+      t.string :color, default: "#000000"
       t.integer :duration
-      t.boolean :payment_required
+      t.boolean :payment_required, default: false
       t.integer :price
       t.references :user, null: false, foreign_key: true
 
