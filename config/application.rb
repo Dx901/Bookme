1b396cb0b483e169b7eeafcbad87fe22683a155e
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-12-10 15:51:14 +0300

    before switch

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+# active_storage
 module Consult
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
