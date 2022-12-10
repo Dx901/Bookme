16de343683878589946a784540aad243fb18ff9e
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-12-10 12:22:38 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
-  cofig.hosts << "rolengiapps.com" 
+  config.hosts << "rolengiapps.com" 
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
